a31ee856f2cf948af27b0de5645c90ec3407d200
basamaryan <basam.aryan@gmail.com>
2025-01-09 15:56:49 -0500

    mt6768-common: media: Import Codec2 configs
    From sunfish TQ3A.230805.001.S1
    
    Change-Id: Ie2ecfa912e0fe0892a2e6b8ddc3a6b6aa3213d40

--- mt6768.mk
+++ mt6768.mk
@@ -272,6 +272,7 @@ PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/media/media_codecs_mediatek_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_mediatek_video.xml \
     $(COMMON_PATH)/configs/media/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
     $(COMMON_PATH)/configs/media/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml
+    $(LOCAL_PATH)/configs/media/media_codecs_performance_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance_c2.xml \
 
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/seccomp/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
