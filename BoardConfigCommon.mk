81b792890b82279f9abac992b2d11c45b8b4b99b
Muichir00 <skyy09321@gmail.com>
2025-04-30 06:27:40 +0800

    mt6768-common: nuke ramework_compatibility_matrix.xml

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -180,8 +180,7 @@ TARGET_VIBRATOR_SUPPORTS_EFFECTS := true
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/configs/vintf/manifest.xml
 DEVICE_MATRIX_FILE += $(COMMON_PATH)/configs/vintf/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-hardware/mediatek/vintf/mediatek_framework_compatibility_matrix.xml \
-$(COMMON_PATH)/configs/vintf/framework_compatibility_matrix.xml
+hardware/mediatek/vintf/mediatek_framework_compatibility_matrix.xml
 
 # Wi-Fi
 WPA_SUPPLICANT_VERSION := VER_0_8_X
